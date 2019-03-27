2eae18734f58676267cfefae5004d912c10c3183
ppl <ppl1121@hotmail.com>
2019-03-27 19:22:27 +0800

    Updates

---
+++ precache-manifest.fe8d51f606ed3c3da20c07155f894cd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff71998fe31970f1baec",
+    "url": "/Google-Font-Playground/static/js/runtime~main.f28b48d3.js"
+  },
+  {
+    "revision": "a0ef5f1fa1785093c39d",
+    "url": "/Google-Font-Playground/static/js/main.dcb11938.chunk.js"
+  },
+  {
+    "revision": "a9fb392f7957c6acc51e",
+    "url": "/Google-Font-Playground/static/js/2.cce3ca6f.chunk.js"
+  },
+  {
+    "revision": "a0ef5f1fa1785093c39d",
+    "url": "/Google-Font-Playground/static/css/main.be5210b7.chunk.css"
+  },
+  {
+    "revision": "5a74f0403217c2321eebc2d682eaf1f3",
+    "url": "/Google-Font-Playground/index.html"
+  }
+];
